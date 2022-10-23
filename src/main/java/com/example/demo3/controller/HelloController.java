cf4db4b839246c0e300a831f811f45b7c2429ce5
includeno <includeno@gmail.com>
2022-10-23 22:06:18 +0800

    init

---
+++ src/main/java/com/example/demo3/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo3.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(String user){
+        return "hello!"+user;
+    }
+}
